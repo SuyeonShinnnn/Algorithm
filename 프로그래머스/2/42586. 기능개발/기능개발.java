4b02727228f29dee328f07af01d18b5e831bb50f
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2025-10-15 14:37:09 +0900

    [level 2] Title: 기능개발, Time: 0.37 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,36 +1,25 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> counts = new ArrayList<>();
-        Queue<Integer> queue = new ArrayDeque<>();
-        
-		    //✅ 작업의 남은 일수를 계산하여 큐에 삽입한다.
-        for (int i = 0; i < progresses.length; i++) {
-            queue.add((int) Math.ceil((100.0 - progresses[i]) / speeds[i]));
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        int[] day = new int[progresses.length];
+        for(int i = 0; i < progresses.length; i++) {
+            day[i] = (int)Math.ceil((double)(100 - progresses[i]) / speeds[i]);
         }
         
-        //✅ 큐가 빌때까지 반복한다.
-        while (!queue.isEmpty()) {
-	        	//✅ 큐에서 요소 하나를 뽑는다.
-            int current = queue.remove();
-          	//✅ 배포 작업 개수를 1으로 초기화한다.
-            int count = 1;
-            
-          	//✅ 큐의 맨 위 요소가 자신보다 작은 동안 요소를 제거하고 작업 개수를 1 늘린다.
-            while (!queue.isEmpty() && queue.peek() <= current) {
-                queue.remove();
-                count++;
+        int max = day[0];
+        int count = 0;
+        List<Integer> answer = new ArrayList<>();
+        for(int d : day) {
+            if(max < d) {
+                max = d;
+                answer.add(count);
+                count = 0;
             }
-          	//✅ 작업 개수를 리스트에 저장한다.
-            counts.add(count);
+            count++;
         }
-        
-        //✅ 작업 개수를 반환한다.
-        int[] answer = new int[counts.size()];
-        for (int i = 0; i < counts.size(); i++) {
-            answer[i] = counts.get(i);
-        }
+        answer.add(count);
+
         return answer;
     }
 }
