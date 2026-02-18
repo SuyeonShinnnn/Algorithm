e60e71cf24cf6269f7d1fc78c50e49dcd228aa04
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-02-18 15:35:07 +0800

    [Silver III] Title: 1로 만들기, Time: 292 ms, Memory: 211976 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,29 +1,31 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{n, 0});
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] curr = q.poll();
-            if(curr[0] == 1) {
+
+            if (curr[0] == 1) {
                 System.out.println(curr[1]);
                 break;
             }
 
-            if(curr[0] % 3 == 0) {
+            if (curr[0] % 3 == 0) {
                 q.offer(new int[]{curr[0] / 3, curr[1] + 1});
             }
-            if(curr[0] % 2 == 0) {
+            if (curr[0] % 2 == 0) {
                 q.offer(new int[]{curr[0] / 2, curr[1] + 1});
             }
+
             q.offer(new int[]{curr[0] - 1, curr[1] + 1});
 
         }
