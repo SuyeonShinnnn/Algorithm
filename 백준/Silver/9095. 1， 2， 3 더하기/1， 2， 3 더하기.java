5253c93b9a520bce5fa31097776653387e57ba7c
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-02-20 21:42:56 +0800

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,33 +1,29 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
-    public static int runDP(int n) {
-        if(n == 0) return 0;
+    public static int dp(int n) {
+        int[] arr = new int[n + 1];
 
-        int[] dp = new int[n + 1];
-
-        for(int i = 1; i < n + 1; i++) {
-            if(i == 1) dp[1] = 1;
-            else if(i == 2) dp[2] = 2;
-            else if(i == 3) dp[3] = 4;
-            else if(i >= 4) {
-                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            }
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) arr[i] = 1;
+            else if (i == 2) arr[i] = 2;
+            else if (i == 3) arr[i] = 4;
+            else arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
         }
-        return dp[n];
+        return arr[n];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(br.readLine());
-        for(int i = 0; i < t; i++) {
-            int input = Integer.parseInt(br.readLine());
-            System.out.println(runDP(input));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int answer = dp(num);
+            System.out.println(answer);
         }
     }
 }
