135d86be7a3d1a005757611b0700a8ed8a42013c
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-03-17 23:55:40 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class SortXY{
+private:
+    int x, y;
+
+public:
+    SortXY(){}
+    SortXY(int x, int y){
+        this -> x = x;
+        this -> y = y;
+    }
+    int getX(){ return x; }
+    int getY() { return y; }
+};
+
+bool compare(SortXY a, SortXY b){
+    if(a.getX() > b.getX()){
+        return false;
+    }
+    else if((a.getX() == b.getX()) && (a.getY() > b.getY())){
+        return false;
+    }
+    else return true;
+}
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int x, y;
+    SortXY sortXY;
+    vector<SortXY> v;
+
+    // 객체 초기화
+    for(int i = 0; i < n; i++){
+        cin >> x >> y;
+        v.push_back(SortXY(x, y));
+    }
+    
+    sort(v.begin(), v.end(), compare);
+
+    for(int i = 0; i < v.size(); i++)
+        cout << v[i].getX() << ' ' << v[i].getY() << "\n";
+}
