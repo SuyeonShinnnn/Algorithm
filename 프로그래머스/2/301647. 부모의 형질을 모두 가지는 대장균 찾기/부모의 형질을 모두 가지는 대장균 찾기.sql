249210b8a4aa27ca218305a7fe7195dc6e3a8fcb
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-05-07 20:02:48 +0900

    Update 부모의 형질을 모두 가지는 대장균 찾기.sql

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,7 +1,3 @@
-# select id, genotype
-# from ecoli_data a
-# where genotype & (select genotype from ecoli_data where id = parent_id) > 0;
-
 select b.id, b.genotype, a.genotype & b.genotype as parent_genotype
 from ecoli_data a, ecoli_data b
 where a.id = b.parent_id and (a.genotype & b.genotype) = a.genotype
