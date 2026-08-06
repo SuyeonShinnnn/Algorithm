3e0651b86165c5646282f26555b066a4677033a0
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-08-06 09:29:50 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,25 +1,29 @@
 import java.util.*;
 
 class Solution {
+    List<Integer> list = new ArrayList<>();
+    
     public List<Integer> solution(int[] progresses, int[] speeds) {
-        int[] day = new int[progresses.length];
         for(int i = 0; i < progresses.length; i++) {
-            day[i] = (int)Math.ceil((double)(100 - progresses[i]) / speeds[i]);
+            int curr = (int)Math.ceil((double)(100 - progresses[i]) / speeds[i]);
+            list.add(curr);
         }
         
-        int max = day[0];
+        int prevMax = list.get(0);
         int count = 0;
         List<Integer> answer = new ArrayList<>();
-        for(int d : day) {
-            if(max < d) {
-                max = d;
-                answer.add(count);
-                count = 0;
+        for(int curr: list) {
+            if(prevMax >= curr) {
+                
+                count++;
+                continue;
             }
-            count++;
+            prevMax = Math.max(prevMax, curr);
+            answer.add(count);
+            count = 1;
         }
-        answer.add(count);
-
+        answer.add(count);    
+        
         return answer;
     }
 }
