83439f3d54480476b0b47f57e959ace114d4faad
SuyeonShinnnn <127740774+SuyeonShinnnn@users.noreply.github.com>
2026-08-17 18:38:44 +0900

    [level 3] Title: 여행경로, Time: 48.74 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
 class Solution {
-    List<String> result = new LinkedList<>();
     
-    public void dfs(int depth, String start, String path, String[][] tickets, boolean[] visited) {
-        if(depth == tickets.length) {
+    private int n;
+    private List<String> result = new ArrayList<>();
+    
+    public void dfs(int depth, String path, String[][] t, String start, boolean[] visited) {
+        if(depth == n) {
             result.add(path);
             return;
         }
         
-        for(int i = 0; i < visited.length; i++) {
-            if(tickets[i][0].equals(start) && !visited[i]) {
+        for(int i = 0; i < n; i++) {
+            if(start.equals(t[i][0]) && !visited[i]) {
                 visited[i] = true;
-                dfs(depth + 1, tickets[i][1], path + " " + tickets[i][1], tickets, visited);
+                dfs(depth + 1, path + " " + t[i][1], t, t[i][1], visited);
                 visited[i] = false;
             }
         }
     }
     public String[] solution(String[][] tickets) {
-        boolean visited[] = new boolean[tickets.length];
-        dfs(0, "ICN", "ICN", tickets, visited);
+        n = tickets.length;
         
+        dfs(0, "ICN", tickets, "ICN", new boolean[n]);
         Collections.sort(result);
+        
         return result.get(0).split(" ");
     }
 }
